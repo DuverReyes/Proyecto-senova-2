484020e55aec39959473a552629fe811652cdc5f
unknown <Sala300@TUNDFPCGE10F370.FormacionSena.RED>
2019-08-06 15:10:18 -0500

    Initial commit

---
+++ ProyectosSENOVA/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ProyectosSENOVA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            int puntaje=0;
+            Proyecto miProyecto = new Proyecto();
+            miProyecto.Area = "sistemas";
+            miProyecto.Codigo = 12000;
+            miProyecto.Duracion = 13;
+            miProyecto.Nombre = "Desarrollo internet de las cosas IOT";
+
+            Console.WriteLine($"Area:{miProyecto.Area}");
+            Console.WriteLine($"Codigo:{miProyecto.Codigo}");
+            Console.WriteLine($"Duracion:{miProyecto.Duracion}");
+            Console.WriteLine($"Nombre:{miProyecto.Nombre}");
+
+            Proyecto miProyecto1 = new Proyecto();
+            miProyecto1.Area = "Desarollo";
+            miProyecto1.Codigo = 14000;
+            miProyecto1.Duracion = 16;
+            miProyecto1.Nombre = "Desarrollo Informacion";
+
+            Console.WriteLine($"Area:{miProyecto1.Area}");
+            Console.WriteLine($"Codigo:{miProyecto1.Codigo}");
+            Console.WriteLine($"Duracion:{miProyecto1.Duracion}");
+            Console.WriteLine($"Nombre:{miProyecto1.Nombre}");
+
+
+            Proyecto miProyecto2 = new Proyecto();
+            Proyecto miProyecto3 = new Proyecto();
+            miProyecto3.IngresarPrpyecto();
+            miProyecto3.ImprimirProyecto();
+
+            Proyecto miProyecto4 = new Proyecto("Realidad Aumentada", 2019, "Tecnologia", 1110);
+            miProyecto4.ImprimirProyecto();
+
+
+            Proyecto miProyecto5 = new Proyecto("Talento Humano", 2020, "SOCIAL", 1220);
+            miProyecto5.ImprimirProyecto();
+
+           
+            Console.ReadLine();
+
+
+            //Entendido...
+
+        }
+    }
+}
---
+++ ProyectosSENOVA/Proyecto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectosSENOVA
+{
+    public class Proyecto
+    {
+        #region Atributos
+
+        #endregion
+        #region Propieades
+        public string Nombre { get; set; }
+        public double Codigo { get; set; }
+        public string Area   { get; set; }
+        public int Duracion  { get; set; }
+        #endregion
+        #region Contructores
+        public Proyecto(string nombre, double codigo, string area, int duracion)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Area = area;
+            Duracion = duracion;
+
+
+
+        }
+        public Proyecto(string area, int duracion)
+        {
+            Area = area;
+            Duracion = duracion;
+        }
+        public Proyecto()
+        {
+
+        }
+        #endregion
+        #region Metodos
+
+        public void IngresarPrpyecto()
+        {
+            Console.WriteLine("Ingrese el Nombre del Proyecto");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el Codigo");
+            Codigo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el Area");
+            Area = Console.ReadLine();
+            Console.WriteLine("Ingrese el Duracion");
+            Duracion = int.Parse(Console.ReadLine());
+
+        }
+
+        public void ImprimirProyecto()
+        {
+            Console.WriteLine($"El Nombre del proyecto es:{Nombre}");
+            Console.WriteLine($"El Codigo del proyecto es:{Codigo}");
+            Console.WriteLine($"El Area del proyecto es:{Area}");
+            Console.WriteLine($"La durecion del proyecto es:{Duracion}");
+        }
+        #endregion
+
+
+    }
+}
